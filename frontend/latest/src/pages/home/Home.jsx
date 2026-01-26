e02f5765b47bf149fd416fdcb8d17b2e571edd4f
rupak karmakar <pradiprup37@gmail.com>
2026-01-27 00:58:52 +0530

    ui design done

--- frontend/latest/src/pages/home/Home.jsx
+++ frontend/latest/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import Sidebar from '../../components/Sidebar/Sidebar.jsx';
-
+import MessageContainer from '../../components/Messages/MessageContainer.jsx';
 
 const Home = () => {
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-white/10 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10'>
       <Sidebar/>
+      <MessageContainer/>
      
     </div>
   );
